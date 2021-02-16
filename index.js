de0051f2bfac49386838843a86f89752492345d9
Carolina R <carolinarizk@gmail.com>
2021-02-16 11:11:39 -0500

    started pseudo

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
+//DEPENDENCIES 
+
 // importing db
   // mysql 
 
+
+// STARTING DATA
 // connection
-  
+
+
+//FUNCTIONS
+  // readItemsTypes 
+  // store items 
+  // 
+
+  //comparison function
+    // to know price
+
+
+
+//USER INTERACTIONS
+  //prompts.js
+
+
+
--- prompts.js
+++ prompts.js
@@ -1,6 +1,21 @@
 // DEPENDENCIES
 // req inquirer
 
-//
 
-// 
+
+// inquirer prompts
+//list: post, bid or exit
+
+// post prompt 
+  //what to post?  item name
+  //what category
+  // price
+
+// bid prompt
+  //choose from items avai for bid
+  //price
+    // reject if lower than posted
+    // if higher, success!
+
+
+
